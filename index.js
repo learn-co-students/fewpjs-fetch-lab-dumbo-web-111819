94510aa799d2236ad6c962edc3a120aa7ac7b525
Soner Mezgitci <you@example.com>
2020-01-08 20:54:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function fetchBooks() {
-
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json))
 }
 
-function renderBooks(json) {
+   function renderBooks(json) {
   const main = document.querySelector('main')
   json.forEach(book => {
     const h2 = document.createElement('h2')
@@ -14,3 +16,4 @@ function renderBooks(json) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 })
+
