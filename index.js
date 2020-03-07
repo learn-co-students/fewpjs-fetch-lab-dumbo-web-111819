742e03aa7d0a36b11f3f3a99f02573e97f6183ed
Soner Mezgitci <you@example.com>
2020-03-06 21:48:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
-   function fetchBooks() {
-  return fetch("https://anapioficeandfire.com/api/books")
+ function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
   .then(r  => r.json())
   .then((response) => {
    renderBooks(response);
@@ -14,9 +14,13 @@
 function renderBooks(json){
     json.forEach(response => {
         let element = document.createElement('li')
-        link.innerHTML = response.name
+        element.innerHTML = response.name
         let main = document.querySelector('main')
-        main.append(element)
+        main.appendChild(element)
         
     });
 }
+
+document.addEventListener('DOMContentLoaded', function(){
+    fetchBooks()
+})
